8c94af3ecd766b7ada634c34edc92e0a94632c5b
keisukekodama <myzk.kodama@gmail.com>
2020-06-28 17:58:41 +0900

    ログアウト時の商品出品

--- sele.rb
+++ sele.rb
@@ -6,5 +6,13 @@ d = Selenium::WebDriver.for :chrome
 url = "https://furima2020.herokuapp.com/" 
 
 d.get(url)
+d.find_element(:class,"purchase-btn").click
+if /ログイン/ .match(d.page_source)
+  puts "ログインしていない状態で商品出品ページへアクセスすると、ログインページへ遷移しました"
+else
+  puts "ログインしていない状態で商品出品ページへアクセスすると、ログインページへ遷移できませんでした"
+end
+d.find_element(:class,"second-logo").click
+
 
 sleep 300
