de30bffbbfefed8ff5f564a9921ea72785840598
keisukekodama <myzk.kodama@gmail.com>
2020-07-03 21:21:20 +0900

    first

--- sele.rb
+++ sele.rb
@@ -2,10 +2,21 @@ require 'selenium-webdriver'
 wait = Selenium::WebDriver::Wait.new(:timeout => 180000)
 d = Selenium::WebDriver.for :chrome
 
+user_nickname = "社会人デビュ"
+user_email = "divde2@co.jp"
+user_password = "aaa111"
+user_first_name = "社会回"
+user_last_name = "出対回"
+user_first_name_kana = "シャカ"
+user_last_name_kana = "デタ"
+user_birth_date_1i = "1998"
+user_birth_date_2i = "9"
+user_birth_date_3i = "30"
+
 # 受講生のURLを記入
 url = "https://furima2020.herokuapp.com/" 
 
-d.get(url)
+ d.get(url)
 d.find_element(:class,"purchase-btn").click
 if /ログイン/ .match(d.page_source)
   puts "◯ログインしていない状態で商品出品ページへアクセスすると、ログインページへ遷移しました"
@@ -52,4 +63,33 @@ else
   wait
 end
 
-sleep 300
+d.find_element(:class,"purchase-btn").click
+
+d.find_element(:class,"sign-up").click
+
+d.find_element(:id, 'user_nickname').send_keys(user_nickname)
+d.find_element(:id, 'user_email').send_keys(user_email)
+d.find_element(:id, 'user_password').send_keys(user_password)
+d.find_element(:id, 'user_password_confirmation').send_keys(user_password)
+d.find_element(:id, 'user_first_name').send_keys(user_first_name)
+d.find_element(:id, 'user_last_name ').send_keys(user_last_name )
+d.find_element(:id, 'user_first_name_kana').send_keys(user_first_name_kana)
+d.find_element(:id, 'user_last_name_kana').send_keys(user_last_name_kana)
+d.find_element(:id, 'user_birth_date_1i').send_keys(user_birth_date_1i)
+d.find_element(:id, 'user_birth_date_2i').send_keys(user_birth_date_2i)
+d.find_element(:id, 'user_birth_date_3i').send_keys(user_birth_date_3i)
+
+d.find_element(:class,"register-red-btn").click
+
+
+if /FURIMAが選ばれる3つの理由/ .match(d.page_source)
+  puts "◯ニックネームが正常に登録されトップページに表示されている。" 
+else
+  puts "☒ニックネームが正常に登録されずに、トップページに表示されない" 
+  wait
+end
+
+
+
+
+sleep 300000000000000
