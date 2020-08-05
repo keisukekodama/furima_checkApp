e3d0a5746ea9c2b8034cf62a704d2d550760e2f3
keisukekodama <myzk.kodama@gmail.com>
2020-08-05 16:26:34 +0900

    id変更

--- sele.rb
+++ sele.rb
@@ -2,23 +2,23 @@ require 'selenium-webdriver'
 wait = Selenium::WebDriver::Wait.new(:timeout => 180000)
 d = Selenium::WebDriver.for :chrome
 
-user_nickname = "ナツ"
-user_email = "divss@co.jp"
-user_password = "aaa111"
-user_first_name = "長蛇"
-user_last_name = "那津"
-user_first_name_kana = "チョウダ"
-user_last_name_kana = "ナツ"
+nickname = "ナツ"
+email = "divss@co.jp"
+password = "aaa111"
+first_name = "長蛇"
+last_name= "那津"
+first_name_kana = "チョウダ"
+last_name_kana = "ナツ"
 user_birth_date_1i = "1991"
 user_birth_date_2i = "2"
 user_birth_date_3i = "10"
 
-user_nickname2 = "ウサギ"
-user_email2 = "dssafdss@co.jp"
-user_first_name2 = "宇佐神"
+nickname2 = "ウサギ"
+email2 = "dssafdss@co.jp"
+first_name2 = "宇佐神"
 user_last_name2 = "心動"
-user_first_name_kana2 = "ウサミ"
-user_last_name_kana2 = "シンドウ"
+first_name_kana2 = "ウサミ"
+last_name_kana2 = "シンドウ"
 user_birth_date_1i2 = "1910"
 user_birth_date_2i2 = "1"
 user_birth_date_3i2 = "11"
@@ -28,27 +28,27 @@ item_image_name = "coat.jpg"
 item_name = "コート"
 item_info = "今年イチオシのトレンチコート"
 item_info_re = "昨年イチオシのトレンチコート"
-item_category_id = "メンズ"
-item_sales_status_id ="目立った傷や汚れなし"
-item_shipping_fee_status_id = "着払い(購入者負担)"
-item_prefecture_id = "山口県"
-item_scheduled_delivery_id = "2~3日で発送"
+item_category = "メンズ"
+item_sales_status ="目立った傷や汚れなし"
+item_shipping_fee_status = "着払い(購入者負担)"
+item_prefecture = "山口県"
+item_scheduled_delivery = "2~3日で発送"
 item_price = 40000
 
 item_image2 = "/Users/tech-camp/furima_checkApp/photo/sunglass.jpg"
 item_name2 = "サングラス"
 item_info2 = "限定5品のサングラス"
-item_category_id2 = "メンズ"
-item_sales_status_id2 ="目立った傷や汚れなし"
-item_shipping_fee_status_id2 = "着払い(購入者負担)"
-item_prefecture_id2 = "群馬県"
-item_scheduled_delivery_id2 = "2~3日で発送"
+item_category2 = "メンズ"
+item_sales_status2 ="目立った傷や汚れなし"
+item_shipping_fee_status2 = "着払い(購入者負担)"
+item_prefecture2 = "群馬県"
+item_scheduled_delivery2 = "2~3日で発送"
 item_price2 = 30000
 
-number = 4242424242424242
-exp_month = 10
-exp_year = 30
-cvc = 123
+card_number = 4242424242424242
+card_exp_month = 10
+card_exp_year = 30
+card_cvc = 123
 postal_code = "965-0873"
 prefecture = "福島県"
 city = "会津若松市" 
@@ -58,45 +58,45 @@ phone_number = "02089001111"
 blank = "---"
 
 #basic認証のidとpass
-b_username = "admin"
-b_password = "2222"
+b_username = "nickname"
+b_password = "anonymous"
 http ="http://#{b_username}:#{b_password}@"
 # 受講生のURLをhttp://以降から記入
-url = "#{http}furima-27004.herokuapp.com/"
-# url = "https://furima2020.herokuapp.com/"
+url = "#{http}furima-28318.herokuapp.com/"
+# url = "http://localhost:3000/"
 
 
 d.get(url)
 
 d.find_element(:class,"sign-up").click
-wait.until {d.find_element(:id, 'user_nickname').displayed?}
-d.find_element(:id, 'user_nickname').send_keys(user_nickname)
-wait.until {d.find_element(:id, 'user_email').displayed?}
-d.find_element(:id, 'user_email').send_keys(user_email)
-wait.until {d.find_element(:id, 'user_password').displayed?}
-d.find_element(:id, 'user_password').send_keys(user_password)
-wait.until {d.find_element(:id, 'user_password_confirmation').displayed?}
-d.find_element(:id, 'user_password_confirmation').send_keys(user_password)
-wait.until {d.find_element(:id, 'user_first_name').displayed?}
-d.find_element(:id, 'user_first_name').send_keys(user_first_name)
-wait.until {d.find_element(:id, 'user_last_name ').displayed?}
-d.find_element(:id, 'user_last_name ').send_keys(user_last_name )
-wait.until {d.find_element(:id, 'user_first_name_kana').displayed?}
-d.find_element(:id, 'user_first_name_kana').send_keys(user_first_name_kana)
-wait.until {d.find_element(:id, 'user_last_name_kana').displayed?}
-d.find_element(:id, 'user_last_name_kana').send_keys(user_last_name_kana)
-wait.until {d.find_element(:id, 'user_birth_date_1i').displayed?}
-d.find_element(:id, 'user_birth_date_1i').send_keys(user_birth_date_1i)
-wait.until {d.find_element(:id, 'user_birth_date_2i').displayed?}
-d.find_element(:id, 'user_birth_date_2i').send_keys(user_birth_date_2i)
-wait.until {d.find_element(:id, 'user_birth_date_3i').displayed?}
-d.find_element(:id, 'user_birth_date_3i').send_keys(user_birth_date_3i)
+wait.until {d.find_element(:id, 'nickname').displayed?}
+d.find_element(:id, 'nickname').send_keys(nickname)
+wait.until {d.find_element(:id, 'email').displayed?}
+d.find_element(:id, 'email').send_keys(email)
+wait.until {d.find_element(:id, 'password').displayed?}
+d.find_element(:id, 'password').send_keys(password)
+wait.until {d.find_element(:id, 'password-confirmation').displayed?}
+d.find_element(:id, 'password-confirmation').send_keys(password)
+wait.until {d.find_element(:id, 'first-name').displayed?}
+d.find_element(:id, 'first-name').send_keys(first_name)
+wait.until {d.find_element(:id, 'last-name').displayed?}
+d.find_element(:id, 'last-name').send_keys(last_name)
+wait.until {d.find_element(:id, 'first-name-kana').displayed?}
+d.find_element(:id, 'first-name-kana').send_keys(first_name_kana)
+wait.until {d.find_element(:id, 'last-name-kana').displayed?}
+d.find_element(:id, 'last-name-kana').send_keys(last_name_kana)
 
-d.find_element(:class,"register-red-btn").click
 
+
+puts "生年月日を入力してください後、登録ボタンを押してください"
+wait.until {d.find_element(:class,"purchase-btn").displayed?}
+
+
+
+##ログイン
 # d.find_element(:class,"login").click 
-# d.find_element(:id, 'user_email').send_keys(user_email)
-# d.find_element(:id, 'user_password').send_keys(user_password)
+# d.find_element(:id, 'email').send_keys(email)
+# d.find_element(:id, 'password').send_keys(password)
 # d.find_element(:class,"login-red-btn").click
 
 
@@ -109,8 +109,8 @@ end
 
 d.find_element(:class,"logout").click
 d.find_element(:class,"login").click 
-d.find_element(:id, 'user_email').send_keys(user_email)
-d.find_element(:id, 'user_password').send_keys(user_password)
+d.find_element(:id, 'email').send_keys(email)
+d.find_element(:id, 'password').send_keys(password)
 d.find_element(:class,"login-red-btn").click
 
 puts "アカウント1でログイン"
@@ -123,29 +123,29 @@ else
 end
 
 d.find_element(:class,"purchase-btn").click
-wait.until {d.find_element(:id,"item_name").displayed?}
-d.find_element(:id,"item_name").send_keys(item_name) 
+wait.until {d.find_element(:id,"item-name").displayed?}
+d.find_element(:id,"item-name").send_keys(item_name) 
 
-wait.until {d.find_element(:id,"item_info").displayed?}
-d.find_element(:id,"item_info").send_keys(item_info)
+wait.until {d.find_element(:id,"item-info").displayed?}
+d.find_element(:id,"item-info").send_keys(item_info)
 
-wait.until {d.find_element(:id,"item_category_id").displayed?}
-d.find_element(:id,"item_category_id").send_keys(item_category_id)
+wait.until {d.find_element(:id,"item-category").displayed?}
+d.find_element(:id,"item-category").send_keys(item_category)
 
-wait.until {d.find_element(:id,"item_sales_status_id").displayed?}
-d.find_element(:id,"item_sales_status_id").send_keys(item_sales_status_id)
+wait.until {d.find_element(:id,"item-sales-status").displayed?}
+d.find_element(:id,"item-sales-status").send_keys(item_sales_status)
 
-wait.until {d.find_element(:id,"item_shipping_fee_status_id").displayed?}
-d.find_element(:id,"item_shipping_fee_status_id").send_keys(item_shipping_fee_status_id)
+wait.until {d.find_element(:id,"item-shipping-fee-status").displayed?}
+d.find_element(:id,"item-shipping-fee-status").send_keys(item_shipping_fee_status)
 
-wait.until {d.find_element(:id,"item_prefecture_id").displayed?}
-d.find_element(:id,"item_prefecture_id").send_keys(item_prefecture_id)
+wait.until {d.find_element(:id,"item-prefecture").displayed?}
+d.find_element(:id,"item-prefecture").send_keys(item_prefecture)
 
-wait.until {d.find_element(:id,"item_scheduled_delivery_id").displayed?}
-d.find_element(:id,"item_scheduled_delivery_id").send_keys(item_scheduled_delivery_id)
+wait.until {d.find_element(:id,"item-scheduled-delivery").displayed?}
+d.find_element(:id,"item-scheduled-delivery").send_keys(item_scheduled_delivery)
 
-wait.until {d.find_element(:id,"item_price").displayed?}
-d.find_element(:id,"item_price").send_keys(item_price)
+wait.until {d.find_element(:id,"item-price").displayed?}
+d.find_element(:id,"item-price").send_keys(item_price)
 
 d.find_element(:class,"sell-btn").click
 
@@ -153,29 +153,29 @@ if /商品の情報を入力/.match(d.page_source)
   puts "◯画像なしで商品出品を行うと、商品出品ページリダイレクトされる" 
 else
   puts "☒画像なしで商品出品を行っても、商品出品ページリダイレクトされない" 
-  wait.until {d.find_element(:id,"item_name").displayed?}
+  wait.until {d.find_element(:id,"item-name").displayed?}
 end
 
 
-d.find_element(:id,"item_name").clear 
-d.find_element(:id,"item_info").clear
-d.find_element(:id,"item_category_id").send_keys(blank)
-d.find_element(:id,"item_sales_status_id").send_keys(blank)
-d.find_element(:id,"item_shipping_fee_status_id").send_keys(blank)
-d.find_element(:id,"item_prefecture_id").send_keys(blank)
-d.find_element(:id,"item_scheduled_delivery_id").send_keys(blank)
-d.find_element(:id,"item_price").clear
+d.find_element(:id,"item-name").clear 
+d.find_element(:id,"item-info").clear
+d.find_element(:id,"item-category").send_keys(blank)
+d.find_element(:id,"item-sales-status").send_keys(blank)
+d.find_element(:id,"item-shipping-fee-status").send_keys(blank)
+d.find_element(:id,"item-prefecture").send_keys(blank)
+d.find_element(:id,"item-scheduled-delivery").send_keys(blank)
+d.find_element(:id,"item-price").clear
 
 
 d.find_element(:id,"item_image").send_keys(item_image)
-d.find_element(:id,"item_name").send_keys(item_name) 
-d.find_element(:id,"item_info").send_keys(item_info)
-d.find_element(:id,"item_category_id").send_keys(item_category_id)
-d.find_element(:id,"item_sales_status_id").send_keys(item_sales_status_id)
+d.find_element(:id,"item-name").send_keys(item_name) 
+d.find_element(:id,"item-info").send_keys(item_info)
+d.find_element(:id,"item-category").send_keys(item_category)
+d.find_element(:id,"item-sales-status").send_keys(item_sales_status)
 
-d.find_element(:id,"item_prefecture_id").send_keys(item_prefecture_id)
-d.find_element(:id,"item_scheduled_delivery_id").send_keys(item_scheduled_delivery_id)
-d.find_element(:id,"item_price").send_keys(item_price)
+d.find_element(:id,"item-prefecture").send_keys(item_prefecture)
+d.find_element(:id,"item-scheduled-delivery").send_keys(item_scheduled_delivery)
+d.find_element(:id,"item-price").send_keys(item_price)
 
 d.find_element(:class,"sell-btn").click
 
@@ -183,29 +183,29 @@ if /商品の情報を入力/.match(d.page_source)
   puts "◯配送料の負担の記入なしで商品出品を行うと、商品出品ページリダイレクトされる" 
 else
   puts "☒配送料の負担の記入なしで商品出品を行っても、商品出品ページリダイレクトされない" 
-  wait.until {d.find_element(:id,"item_name").displayed?}
+  wait.until {d.find_element(:id,"item-name").displayed?}
 end
 
 
 d.find_element(:id,"item_image").clear 
-d.find_element(:id,"item_name").clear 
-d.find_element(:id,"item_info").clear
-d.find_element(:id,"item_category_id").send_keys(blank)
-d.find_element(:id,"item_sales_status_id").send_keys(blank)
-d.find_element(:id,"item_shipping_fee_status_id").send_keys(blank)
-d.find_element(:id,"item_prefecture_id").send_keys(blank)
-d.find_element(:id,"item_scheduled_delivery_id").send_keys(blank)
-d.find_element(:id,"item_price").clear
+d.find_element(:id,"item-name").clear 
+d.find_element(:id,"item-info").clear
+d.find_element(:id,"item-category").send_keys(blank)
+d.find_element(:id,"item-sales-status").send_keys(blank)
+d.find_element(:id,"item-shipping-fee-status").send_keys(blank)
+d.find_element(:id,"item-prefecture").send_keys(blank)
+d.find_element(:id,"item-scheduled-delivery").send_keys(blank)
+d.find_element(:id,"item-price").clear
 
 d.find_element(:id,"item_image").send_keys(item_image)
-d.find_element(:id,"item_name").send_keys(item_name) 
-d.find_element(:id,"item_info").send_keys(item_info)
-d.find_element(:id,"item_category_id").send_keys(item_category_id)
-d.find_element(:id,"item_sales_status_id").send_keys(item_sales_status_id)
-d.find_element(:id,"item_shipping_fee_status_id").send_keys(item_shipping_fee_status_id)
-d.find_element(:id,"item_prefecture_id").send_keys(item_prefecture_id)
-d.find_element(:id,"item_scheduled_delivery_id").send_keys(item_scheduled_delivery_id)
-d.find_element(:id,"item_price").send_keys(item_price)
+d.find_element(:id,"item-name").send_keys(item_name) 
+d.find_element(:id,"item-info").send_keys(item_info)
+d.find_element(:id,"item-category").send_keys(item_category)
+d.find_element(:id,"item-sales-status").send_keys(item_sales_status)
+d.find_element(:id,"item-shipping-fee-status").send_keys(item_shipping_fee_status)
+d.find_element(:id,"item-prefecture").send_keys(item_prefecture)
+d.find_element(:id,"item-scheduled-delivery").send_keys(item_scheduled_delivery)
+d.find_element(:id,"item-price").send_keys(item_price)
 
 d.find_element(:class,"sell-btn").click
 
@@ -260,7 +260,7 @@ end
 
 d.find_element(:class,"item-red-btn").click
 
-d.find_element(:id,"item_info").clear
+d.find_element(:id,"item-info").clear
 d.find_element(:class,"sell-btn").click
 if /商品の情報を入力/.match(d.page_source)
   puts "◯無効な情報で商品編集を行うと、商品出品ページリダイレクトされる" 
@@ -269,7 +269,7 @@ else
   wait.until {d.find_element(:id,"sell-btn").displayed?}
 end
 
-d.find_element(:id,"item_info").send_keys(item_info_re)
+d.find_element(:id,"item-info").send_keys(item_info_re)
 d.find_element(:class,"sell-btn").click
 
 if /#{item_info_re}/.match(d.page_source)
@@ -352,42 +352,42 @@ end
 
 d.find_element(:class,"second-logo").click
 d.find_element(:class,"sign-up").click
-d.find_element(:id, 'user_nickname').send_keys(user_nickname2)
-d.find_element(:id, 'user_email').send_keys(user_email2)
-d.find_element(:id, 'user_password').send_keys(user_password)
-d.find_element(:id, 'user_password_confirmation').send_keys(user_password)
-d.find_element(:id, 'user_first_name').send_keys(user_first_name2)
-d.find_element(:id, 'user_last_name ').send_keys(user_last_name2)
-d.find_element(:id, 'user_first_name_kana').send_keys(user_first_name_kana2)
-d.find_element(:id, 'user_last_name_kana').send_keys(user_last_name_kana2)
-d.find_element(:id, 'user_birth_date_1i').send_keys(user_birth_date_1i2)
-d.find_element(:id, 'user_birth_date_2i').send_keys(user_birth_date_2i2)
-d.find_element(:id, 'user_birth_date_3i').send_keys(user_birth_date_3i2)
-d.find_element(:class,"register-red-btn").click
+d.find_element(:id, 'nickname').send_keys(nickname2)
+d.find_element(:id, 'email').send_keys(email2)
+d.find_element(:id, 'password').send_keys(password)
+d.find_element(:id, 'password-confirmation').send_keys(password)
+d.find_element(:id, 'first-name').send_keys(first_name2)
+d.find_element(:id, 'last-name').send_keys(user_last_name2)
+d.find_element(:id, 'first-name-kana').send_keys(first_name_kana2)
+d.find_element(:id, 'last-name-kana').send_keys(last_name_kana2)
 
+puts "生年月日を入力してください後、登録ボタンを押してください"
+wait.until {d.find_element(:class,"purchase-btn").displayed?}
+
+##ログイン
 # d.find_element(:class,"login").click
-# d.find_element(:id, 'user_email').send_keys(user_email2)
-# d.find_element(:id, 'user_password').send_keys(user_password)
+# d.find_element(:id, 'email').send_keys(email2)
+# d.find_element(:id, 'password').send_keys(password)
 # d.find_element(:class,"login-red-btn").click
 
 
 
 
 #クレジットカード情報入力画面に遷移
-wait.until {d.find_element(:id, 'number').displayed?}
-d.find_element(:id, 'number').send_keys(number)
+wait.until {d.find_element(:id, 'card-number').displayed?}
+d.find_element(:id, 'card-number').send_keys(card_number)
 
-wait.until {d.find_element(:id, 'exp_month').displayed?}
-d.find_element(:id, 'exp_month').send_keys(exp_month)
+wait.until {d.find_element(:id, 'card-exp-month').displayed?}
+d.find_element(:id, 'card-exp-month').send_keys(card_exp_month)
 
-wait.until {d.find_element(:id, 'exp_year').displayed?}
-d.find_element(:id, 'exp_year').send_keys(exp_year)
+wait.until {d.find_element(:id, 'card-exp-year').displayed?}
+d.find_element(:id, 'card-exp-year').send_keys(card_exp_year)
 
-wait.until {d.find_element(:id, 'cvc').displayed?}
-d.find_element(:id, 'cvc').send_keys(cvc)
+wait.until {d.find_element(:id, 'card-cvc').displayed?}
+d.find_element(:id, 'card-cvc').send_keys(card_cvc)
 
-wait.until {d.find_element(:id, 'postal_code').displayed?}
-d.find_element(:id, 'postal_code').send_keys(postal_code)
+wait.until {d.find_element(:id, 'postal-code').displayed?}
+d.find_element(:id, 'postal-code').send_keys(postal_code)
 
 wait.until {d.find_element(:id, 'prefecture').displayed?}
 d.find_element(:id, 'prefecture').send_keys(prefecture)
@@ -398,8 +398,8 @@ d.find_element(:id, 'city').send_keys(city)
 wait.until {d.find_element(:id, 'addresses').displayed?}
 d.find_element(:id, 'addresses').send_keys(addresses)
 
-wait.until {d.find_element(:id, 'phone_number').displayed?}
-d.find_element(:id, 'phone_number').send_keys(phone_number)
+wait.until {d.find_element(:id, 'phone-number').displayed?}
+d.find_element(:id, 'phone-number').send_keys(phone_number)
 
 d.find_element(:class,"buy-red-btn").click
 puts "クレジットカード情報登録完了"
@@ -426,14 +426,14 @@ d.find_element(:class,"furima-icon").click
 
 d.find_element(:class,"purchase-btn").click
 d.find_element(:id,"item_image").send_keys(item_image2)
-d.find_element(:id,"item_name").send_keys(item_name2) 
-d.find_element(:id,"item_info").send_keys(item_info2)
-d.find_element(:id,"item_category_id").send_keys(item_category_id2)
-d.find_element(:id,"item_sales_status_id").send_keys(item_sales_status_id2)
-d.find_element(:id,"item_shipping_fee_status_id").send_keys(item_shipping_fee_status_id2)
-d.find_element(:id,"item_prefecture_id").send_keys(item_prefecture_id2)
-d.find_element(:id,"item_scheduled_delivery_id").send_keys(item_scheduled_delivery_id2)
-d.find_element(:id,"item_price").send_keys(item_price2)
+d.find_element(:id,"item-name").send_keys(item_name2) 
+d.find_element(:id,"item-info").send_keys(item_info2)
+d.find_element(:id,"item-category").send_keys(item_category2)
+d.find_element(:id,"item-sales-status").send_keys(item_sales_status2)
+d.find_element(:id,"item-shipping-fee-status").send_keys(item_shipping_fee_status2)
+d.find_element(:id,"item-prefecture").send_keys(item_prefecture2)
+d.find_element(:id,"item-scheduled-delivery").send_keys(item_scheduled_delivery2)
+d.find_element(:id,"item-price").send_keys(item_price2)
 d.find_element(:class,"sell-btn").click
 
 d.find_element(:class,"item-img-content").click 
