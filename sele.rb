f7aa31ee31b7a5082d5da7d45673b0fbf5b51117
keisukekodama <myzk.kodama@gmail.com>
2020-08-22 17:17:48 +0900

    修正

--- sele.rb
+++ sele.rb
@@ -2,23 +2,23 @@ require 'selenium-webdriver'
 wait = Selenium::WebDriver::Wait.new(:timeout => 180000)
 d = Selenium::WebDriver.for :chrome
 
-nickname = "ナツ"
+nickname = "ク・スンジュン"
 email = "divs2s@co.jp"
 password = "aaa111"
-first_name = "長末蛇"
-last_name= "那津葉"
-first_name_kana = "タョウダ"
-last_name_kana = "ナツテ"
+first_name = "愛"
+last_name= "不時着"
+first_name_kana = "アイ"
+last_name_kana = "フジチャク"
 user_birth_date_1i = "1991"
 user_birth_date_2i = "2"
 user_birth_date_3i = "10"
 
-nickname2 = "ウサギ"
+nickname2 = "クラス"
 email2 = "dssafdss@co.jp"
-first_name2 = "宇佐神"
-user_last_name2 = "心動"
-first_name_kana2 = "ウサミ"
-last_name_kana2 = "シンドウ"
+first_name2 = "梨泰"
+user_last_name2 = "院"
+first_name_kana2 = "イテウォン"
+last_name_kana2 = "クラス"
 user_birth_date_1i2 = "1910"
 user_birth_date_2i2 = "1"
 user_birth_date_3i2 = "11"
@@ -58,11 +58,11 @@ phone_number = "02089001111"
 blank = "---"
 
 #basic認証のidとpass
-b_username = "kentaro"
-b_password = "1997"
+b_username = "furima"
+b_password = "8910"
 http ="http://#{b_username}:#{b_password}@"
 # 受講生のURLをhttp://以降から記入
-# url = "#{http}furima-28375.herokuapp.com/"
+# url = "#{http}furima-28690.herokuapp.com/"
 url = "http://localhost:3000/"
 
 
@@ -163,10 +163,19 @@ if /FURIMAが選ばれる3つの理由/ .match(d.page_source)
   puts "◯ログイン/ログアウトができる" 
 else
   puts "☒ログイン/ログアウトができない" 
-  wait.until {d.find_element(:class,"purchase-btn").displayed?}
+  wait.until {d.find_element(:class,"purchase-btn-text").displayed?}
 end
 
-d.find_element(:class,"purchase-btn").click
+d.find_element(:class,"purchase-btn-text").click
+
+
+if /purchase-btn/.match(d.page_source)
+  d.find_element(:class,"purchase-btn").click
+else
+  
+end
+
+
 wait.until {d.find_element(:id,"item-name").displayed?}
 d.find_element(:id,"item-name").send_keys(item_name) 
 
@@ -258,7 +267,7 @@ if /#{item_name}/ .match(d.page_source)
   puts "!有効な情報を入力するとトップページへ遷移し、商品名が表示されている。" 
 else
   puts "!有効な情報を入力しても、商品名が表示されない。" 
-  wait.until {d.find_element(:class,"purchase-btn").displayed?}
+  wait.until {d.find_element(:class,"purchase-btn-text").displayed?}
 end
 
 
@@ -267,7 +276,7 @@ if /#{item_image_name}/ .match(d.page_source)
   puts "！有効な情報を入力するとトップページへ遷移し、画像が表示されている。" 
 else
   puts "!有効な情報を入力しても、画像が表示されない" 
-  wait.until {d.find_element(:class,"purchase-btn").displayed?}
+  wait.until {d.find_element(:class,"purchase-btn-text").displayed?}
 end
 
 
@@ -276,13 +285,14 @@ if /#{item_price}/.match(d.page_source)
   puts "!有効な情報を入力するとトップページへ遷移し、商品価格が表示されている。" 
 else
   puts "!有効な情報を入力しても商品価格が表示されない" 
-  wait.until {d.find_element(:class,"purchase-btn").displayed?}
+  wait.until {d.find_element(:class,"purchase-btn-text").displayed?}
 end
 
 puts "◯必須項目を入力した上で出品ができる"
 puts "◯出品した商品の一覧表示ができている"
 puts "◯「画像/価格/商品名」の3つの情報について表示できている"
 
+sleep 2
 d.find_element(:class,"item-img-content").click 
 
 if /編集/.match(d.page_source)
@@ -320,7 +330,7 @@ end
 
 d.find_element(:id,"item-info").send_keys(item_info_re)
 d.find_element(:class,"sell-btn").click
-
+sleep 1
 if /#{item_info_re}/.match(d.page_source)
   puts "!有効な情報で商品編集を行うと、レコードが更新され、商品詳細ページへ遷移し、商品出品時に入力した値が表示されている" 
 else
@@ -334,7 +344,12 @@ d.find_element(:class,"furima-icon").click
 
 
 d.find_element(:class,"logout").click
-d.find_element(:class,"purchase-btn").click
+d.find_element(:class,"purchase-btn-text").click
+if /purchase-btn/.match(d.page_source)
+  d.find_element(:class,"purchase-btn").click
+else
+  
+end
 if /ログイン/ .match(d.page_source)
   puts "!ログインしていない状態で商品出品ページへアクセスすると、ログインページへ遷移しました"
 else
@@ -360,62 +375,35 @@ puts "!商品価格は" + item_price.text
 
 d.find_element(:class,"item-img-content").click
 if /編集/ .match(d.page_source)
-  puts "☒ログインしていないユーザーでも、商品の編集が行える" 
+  puts "!ログインしていないユーザーでも、商品の編集が行える" 
   wait.until {d.find_element(:class,"item-red-btn").displayed?}
 else
-  puts "◯ログインしていないユーザーは、商品の編集が行えない。" 
+  puts "!ログインしていないユーザーは、商品の編集が行えない。" 
 end
 
 if /削除/ .match(d.page_source)
-  puts "☒ログインしていないユーザーでも、商品の削除が行える"
+  puts "!ログインしていないユーザーでも、商品の削除が行える"
   wait.until {d.find_element(:class,"item-red-btn").displayed?}
 else
-  puts "◯ログインしていないユーザーは、商品の削除が行えない。" 
+  puts "!ログインしていないユーザーは、商品の削除が行えない。" 
 end
 
 
+sleep 1
 
-# if /購入画面に進む/.match(d.page_source)
-#   puts "◯ログインした上で自分が出品していない商品詳細ページへアクセスした場合は、「購入」のリンクが表示される" 
-# else
-#   puts "☒ログインした上で自分が出品していない商品詳細ページへアクセスした場合に、「購入」のリンクが表示されない" 
-#   wait.until {d.find_element(:class,"item-red-btn").displayed?}
-# end
-
-sleep 2
-
-
-if /編集/ .match(d.page_source)
-  puts "!ログインした上で自分が出品していない商品詳細ページへアクセスした場合に、「編集」のリンクが表示される" 
-  wait.until {d.find_element(:class,"item-red-btn").displayed?}
-else
-  puts "!ログインした上で自分が出品していない商品詳細ページへアクセスした場合は、「編集」のリンクが表示されない" 
-end
-
-puts "◯出品者だけが編集ページに遷移できる"
 puts "◯ログアウトした状態でも、商品詳細ページを閲覧できる"
-puts "◯出品者にしか、商品編集・削除のリンクが踏めないようになっている"
 
-if /削除/ .match(d.page_source)
-  puts "☒ログインした上で自分が出品していない商品詳細ページへアクセスした場合に、「削除」のリンクが表示される" 
-  wait.until {d.find_element(:class,"item-red-btn").displayed?}
-else
-  puts "◯ログインした上で自分が出品していない商品詳細ページへアクセスした場合は、「削除」のリンクが表示されない" 
-end
+
+
 
 sleep 2
 puts "【説明】購入ボタン自体を消しているてる場合があるので一度、サインアップする"
-# d.find_element(:class,"item-red-btn").click
 
-# if /会員情報入力/ .match(d.page_source)
-#   puts "!ログインしていない状態で購入ボタンを押すとログインページに遷移する。" 
-# else
-#   puts "!ログインしていない状態で購入ボタンを押すとログインページに遷移しない。" 
-#   wait.until {d.find_element(:id,"email").displayed?}
-# end
 
 sleep 1
+d.manage.delete_all_cookies
 d.find_element(:class,"sign-up").click
+
 puts "新しいユーザーを登録する"
 d.find_element(:id, 'nickname').send_keys(nickname2)
 d.find_element(:id, 'email').send_keys(email2)
@@ -426,20 +414,41 @@ d.find_element(:id, 'last-name').send_keys(user_last_name2)
 d.find_element(:id, 'first-name-kana').send_keys(first_name_kana2)
 d.find_element(:id, 'last-name-kana').send_keys(last_name_kana2)
 puts "生年月日を入力してください後、登録ボタンを押してください"
-wait.until {d.find_element(:class, 'new-items').displayed?}
+
+
+wait.until {d.find_element(:class,"purchase-btn").displayed?}
 puts "【説明】商品出品ページに遷移してしまうためトップページに遷移後、商品購入画面に遷移する。"
 
-d.find_element(:class,"items-sell-header").click
-d.find_element(:class,"item-img-content").click
-d.find_element(:class,"item-red-btn").click
-
 # #ログイン
+# sleep 1
 # d.find_element(:class,"login").click
 # d.find_element(:id, 'email').send_keys(email2)
 # d.find_element(:id, 'password').send_keys(password)
 # d.find_element(:class,"login-red-btn").click
+# d.find_element(:class,"items-sell-header").click
+# d.find_element(:class,"item-img-content").click
+# d.find_element(:class,"item-red-btn").click
+# sleep 1
 
-sleep 1
+
+d.find_element(:class,"item-img-content").click
+if /編集/ .match(d.page_source)
+  puts "!ログインした上で自分が出品していない商品詳細ページへアクセスした場合に、「編集」のリンクが表示される" 
+  wait.until {d.find_element(:class,"item-red-btn").displayed?}
+else
+  puts "!ログインした上で自分が出品していない商品詳細ページへアクセスした場合は、「編集」のリンクが表示されない" 
+end
+
+
+if /削除/ .match(d.page_source)
+  puts "!ログインした上で自分が出品していない商品詳細ページへアクセスした場合に、「削除」のリンクが表示される" 
+  wait.until {d.find_element(:class,"item-red-btn").displayed?}
+else
+  puts "!ログインした上で自分が出品していない商品詳細ページへアクセスした場合は、「削除」のリンクが表示されない" 
+end
+puts "◯出品者にしか、商品編集・削除のリンクが踏めないようになっている"
+puts "◯出品者だけが編集ページに遷移できる"
+d.find_element(:class,"item-red-btn").click
 
 
 
@@ -501,6 +510,7 @@ end
 
 d.find_element(:class,"furima-icon").click 
 
+d.find_element(:class,"purchase-btn-text").click
 d.find_element(:class,"purchase-btn").click
 d.find_element(:id,"item-image").send_keys(item_image2)
 d.find_element(:id,"item-name").send_keys(item_name2) 
@@ -520,7 +530,7 @@ d.find_element(:class,"item-destroy").click
 
 if /#{item_name2}/ .match(d.page_source)
   puts "☒出品者だけが商品情報を削除できない" 
-  wait.until {d.find_element(:class,"purchase-btn").displayed?}
+  wait.until {d.find_element(:class,"purchase-btn-text").displayed?}
 else
   puts "◯出品者だけが商品情報を削除できる" 
 end
